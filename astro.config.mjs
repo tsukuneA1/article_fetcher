2f21ddd5091563650455f73659bbe8f7ba536bb6
A1 <tkschr1125@gmail.com>
2025-02-16 23:55:09 +0900

    add site

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,4 +2,6 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+    'site': 'https://article-fetcher-mauve.vercel.app/',
+});
---
+++ src/pages/feed.ts
@@ -0,0 +1,19 @@
+import rss from '@astrojs/rss'
+import { getAllPosts } from '../libs/micro';
+import type { Post } from '../libs/interfaces';
+
+export async function GET() {
+  const posts: Post[] = await getAllPosts();
+
+  return rss({
+    title: 'posts',
+    description: 'posts data',
+    site: import.meta.env.SITE,
+    items: posts.map((post) => ({
+      link: post.Link,
+      title: post.Title,
+      description: post.Excerpt,
+      pubDate: new Date(post.Date),
+    })),
+  })
+}
