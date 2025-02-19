680a67d429b24031f1efb2a0b7814ed2923b106c
A1 <tkschr1125@gmail.com>
2025-02-19 15:33:46 +0900

    JSON parse errorが出たのでもともとちゃんと動いてた部分はそのままにした

--- src/pages/feed.ts
+++ src/pages/feed.ts
@@ -1,11 +1,9 @@
 import rss from '@astrojs/rss'
+import { getAllPosts } from '../libs/micro';
 import type { Post } from '../libs/interfaces';
 
 export async function GET() {
-  const siteUrl = import.meta.env.SITE_URL as string;
-
-const response = await fetch(`${siteUrl}/api/posts`);
-  const posts: Post[] = await response.json();
+  const posts: Post[] = await getAllPosts();
 
   return rss({
     title: 'posts',
